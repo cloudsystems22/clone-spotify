9d10170e32bd8afcec82e5d0d1dd156ec2b0c732
cloudsystems22 <cloudsystems22@gmail.com>
2020-11-05 20:24:16 -0300

    Primeiras impressões

---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+import "./style.css"
+
+const Header = () => {
+  return (
+    <header>
+        <h1 className="header-h1">Spotify</h1>
+        <nav>
+            <ul>
+                <li>Premium</li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
+
+export default Header;
